1e20106687272a2be9ab66cc5a6647047a3d5532
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-12-01 14:37:37 -0300

    string

---
+++ Ruby fundamentos/006_string.rb
@@ -0,0 +1,21 @@
+# Dados
+
+name = "Hisstrahr"
+
+name2 = "hisstrahr"
+
+# Ruby é case sensitive
+
+puts name == name2
+
+des = "Hiss é uma pessoa complicada"
+des2 = <<Text # Aqui trabalhamos com multilinha
+  O #{name} é uma pessoa complicada 
+Text
+
+puts des2
+
+# Para verificar o tamanho da string
+
+puts des2.length 
+puts des.size
