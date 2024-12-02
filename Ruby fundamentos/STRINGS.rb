af948fb2d93b5be36127c0f279209c98f96063cf
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-12-02 17:50:29 -0300

    att

---
+++ Ruby fundamentos/STRINGS.rb
@@ -0,0 +1,23 @@
+puts "Informe o seu primeiro nome: "
+
+nome1 = gets.chomp.downcase
+
+puts "Informe o seu segundo nome: "
+
+nome2 = gets.chomp.downcase
+
+puts "Informe o nome da sua companhia: "
+
+company = gets.chomp.downcase
+
+ponto = "."
+var1 = "@"
+var2 = ".com"
+
+result = nome1+ponto+nome2+var1+company+var2
+
+print result
+
+
+
+
