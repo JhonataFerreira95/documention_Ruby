fb2c0660d28297e382a657f5a81c89c9c0635179
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-12-03 16:51:52 -0300

    att

--- Ruby fundamentos/STRINGS.rb
+++ Ruby fundamentos/STRINGS.rb
@@ -18,6 +18,25 @@ result = nome1+ponto+nome2+var1+company+var2
 
 print result
 
+# Gerador de E-mail REFATORANDO
+
+puts "Informe o seu primeiro nome: "
+
+nome1 = gets.chomp
+
+puts "Informe o seu segundo nome: "
+
+nome2 = gets.chomp
+
+puts "Informe o nome da sua companhia: "
+
+company = gets.chomp
+
+email = ""
+
+email << name.downcase.split.join(".")
+
+
 
 
 
