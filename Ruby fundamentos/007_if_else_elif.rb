0a797899fc36cbb0e50ded792e12db19114bc946
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-12-12 13:35:39 -0300

    Trabalhando com if else

--- Ruby fundamentos/007_if_else_elif.rb
+++ Ruby fundamentos/007_if_else_elif.rb
@@ -3,6 +3,9 @@ b = 200
 
 # Para verificar se a > b, b < a ou se b > a. utilizamos as condicionais.
 
-if a > b :"Se #{a} for maior que #{b}"{
-  
-}
+if a > b
+    puts "#{a} maior que #{b}"
+else
+    puts "#{b} maior que #{a}"
+end
+
