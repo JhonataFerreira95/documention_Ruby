8d2c6e465b5c13138ca64164623bcaaa5991cfb8
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-01 16:37:31 -0300

    att

--- Ruby fundamentos/012_metodos.rb
+++ Ruby fundamentos/012_metodos.rb
@@ -1,6 +1,8 @@
 # 1-Método para imprimir Hello World
 def Hello
+
   puts "Hello World"
+  
 end
 
 # Hello()
@@ -9,15 +11,20 @@ end
 # Hello()
 
 # 2-Método para somar dois números
+
 def sum()
+
   puts 5 + 4
+
 end
 
 # sum()
 # sum()
 
 # 3-Método para Cadastrar Filme
+
 def create_movie
+
   puts "Digite o nome do filme:"
   name = gets.chomp
   puts "Digite o ano de lançamento do filme:"
@@ -26,6 +33,7 @@ def create_movie
   price = gets.chomp.to_f
 
   puts "#{name} - R$ #{price}"
+
 end
 
 create_movie()
