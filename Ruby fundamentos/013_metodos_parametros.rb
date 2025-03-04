cf902edc3dbb29a7b7fc8d618f310501b469fd97
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-04 16:23:28 -0300

    adc blocks and lambda

--- Ruby fundamentos/013_metodos_parametros.rb
+++ Ruby fundamentos/013_metodos_parametros.rb
@@ -1,27 +1,35 @@
 # 1-Método que recebe dois parâmetros sobre nome
+# Este método recebe dois parâmetros, fname e lname, e imprime o nome completo.
 def full_name(fname, lname)
   puts "Nome completo: #{fname} #{lname}"
 end
 
+# Chamadas de exemplo para o método full_name
 full_name("Rodrigo", "Macedo")
 full_name("Sicrano", "Fulano")
 
 # 2-Método que soma dois números
+# Este método recebe dois parâmetros, a e b, e retorna a soma deles.
 def sum(a, b)
   return a + b
 end
 
+# Exemplo de chamada para o método sum
 puts sum(10, 20)
 
 # 3-Argumentos default num método
+# Este método recebe um parâmetro opcional country, com valor padrão "Brasil".
 def address(country="Brasil")
   puts "Eu moro no #{country}"
 end
 
+# Chamadas de exemplo para o método address
 address()
 address("Portugal")
 
 # 4-Avaliação do Filme
+# Este método recebe um parâmetro qtdRating, que indica a quantidade de avaliações.
+# Ele solicita o nome do filme e as notas, calcula a média e a exibe.
 def rating_movie(qtdRating)
   puts "Informe o nome do filme:"
   movie_name = gets.chomp
@@ -34,15 +42,16 @@ def rating_movie(qtdRating)
   puts "Média de avaliação do filme #{movie_name} é: #{sum / qtdRating}"
 end
 
+# Solicita ao usuário a quantidade de avaliações e chama o método rating_movie
 puts "Deseja fazer quantas avaliações:"
 rating = gets.chomp.to_i
 
 rating_movie(rating)
 
 # 5-avaliação do filme versão 2
-
+# Este método é uma versão alternativa do método de avaliação de filmes.
+# Ele recebe um parâmetro avaliacao, solicita o nome do filme e as notas, calcula a média e a exibe.
 def av(avaliacao)
-  
     puts "Informe o nome do filme: "
     movie_name = gets.chomp
     sum = 0 
@@ -55,6 +64,7 @@ def av(avaliacao)
     puts "Média de avaliação do filme #{movie_name} é: %2.f" %media
 end
 
+# Solicita ao usuário a quantidade de avaliações e chama o método av
 puts "Deseja fazer quantas avaliações: "
 qtAv = gets.chomp.to_i
 
---
+++ Ruby fundamentos/014_bloacks_lambda.rb
@@ -0,0 +1,51 @@
+# 1-Blocks, o que são? Blocos são trechos anônimos de código que aceitam
+# entradas de argumentos e retornam um determinado valor.
+
+def hello
+  yield # yield é uma palavra reservada que chama o bloco.
+end
+
+hello {puts "Olá, mundo!"}
+hello {puts "Olá, mundo 2!"}
+hello {puts "Olá, mundo 3!"}
+
+# Exemplo 2
+
+def one_two_trhee
+  yield 1
+  yield 2
+  yield 3
+end
+
+one_two_trhee {|number| puts number * 10} 
+
+=begin
+  
+Aqui vou aborda um pouco o usor de pipes, que são usados para passar argumentos para o bloco acima.
+
+Em Ruby, os pipes | | são usados para definir parâmetros de bloco.
+Eles permitem que você passe argumentos para o bloco de código que está sendo executado.
+No exemplo one_two_trhee {|number| puts number * 10}, |number| indica que o bloco aceita 
+um argumento chamado number.
+
+=end
+
+# 2-Lambda, o que é? Lambda é uma função anônima que pode ter qualquer número de argumentos, mas
+# retorna apenas um valor.  
+
+power = lambda { |num| num ** 2 } #Função de potência de um número.
+puts power.call(4) # Retorna 16 no terminal.
+
+# Função de soma de dois são par.
+
+funpar = lambda { |par| par % 2 == 0 } 
+puts funpar.call(5) # Retorna false no terminal, já que resto da divisão 5/2=1.
+
+# Função que divide um número por outro.
+
+div = lambda { |a, b| a / b} 
+puts div.call(20, 10)# Retorna no console o resultado da divisão de 20/10=2.
+
+# Se perguntou o porque se chama função lambda? Te explico.
+# Ela não tem um nome definido, é geralmente atribuída para uma variável/referência.
+# Depois você chama sua variável e passa o seus devidos parâmentros.
