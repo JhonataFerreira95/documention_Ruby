4bfab7356017c4148d9a424578b33d4e17da3c25
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-08 19:56:21 -0300

    finalizado hash

--- Ruby fundamentos/017_hash.rb
+++ Ruby fundamentos/017_hash.rb
@@ -22,3 +22,19 @@ puts movie2.values # O método keys e values. Imprime as valores no terminal com
 
 # Iterando com for.
 
+for key, value in movie2 # Recuperando valores está recuperando e exibindo os valores do hash movie2.
+    puts "#{key}-#{value}" # Imprime na tela cada chave e seu valor correspondente no formato, <key> representa a chave de cada par do hash, <value> representa o valor associado a essa chave.
+end
+
+# Iterando com while.
+
+i = 0 # Váriavel de controle.
+
+while  i < movie2.lenght # Aqui o <lenght> verifica se o i é menor que o nosso hash.
+    puts "#{movie2.keys[i]}-#{movie2.values[i]}" # Imprimir os valores no terminal correspondente <key> e <values>.
+    i += 1 # Para que o laço não seja infinito.
+end
+
+# Interando valores com each.
+
+movie2.each { |key, value| puts "#{key}-#{value}"} # Imprimir os valores no terminal correspondente <key> e <value>.
