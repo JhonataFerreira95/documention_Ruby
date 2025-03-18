57309d9fb9b6cc8af398c28c20fe5a00a0d22b0e
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-18 02:10:14 -0300

    att

--- Ruby arquivos, módulos e gems/Módulos/002_write_file.rb
+++ Ruby arquivos, módulos e gems/Módulos/002_write_file.rb
@@ -3,7 +3,7 @@
 # Alternativa 1.
 
 # Aqui utilizamos o <File.open> para abrir o arquivo. Como estamos trabalhando com modo de escrita, não precisamo que o arquivos exista, já que iremos criar o mesmo.
-File.open("../Manipulação de arquivos/Text/courses.txt", "w") do |file| # Arquivo <data> não existe AINDA. Quando passamo o <"w"> significa que quero trabalha com write no ruby. 
+File.open("../Manipulação de arquivos/Text/courses.txt", "w") do |file| # Arquivo <data> não existe AINDA. Quando passamo o <"w"> significa que quero trabalha com (write == escrever) no ruby. 
   file.puts "Curso de OnRails"     #  Criamos um bloco com <do> passamos nos pipes <||> nossa variável que é <|file| e chamamos os arquivos de texto que serão criados.
   file.write "Curso de Gems" # Diferença entre o <puts> e o <write> é meramente uma quebra de linha, já que o <puts> possuí nativamente a quebra de linha e o <write> não.
   file.puts "Curso de UML"
