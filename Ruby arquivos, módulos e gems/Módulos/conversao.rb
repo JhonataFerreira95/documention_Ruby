4853a1709f0eb8c0800f744b4ed1312c4b4c2b7e
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-20 15:28:03 -0300

    corrigido erro na linha 12, foi trocado o . por _

--- Ruby arquivos, módulos e gems/Módulos/conversao.rb
+++ Ruby arquivos, módulos e gems/Módulos/conversao.rb
@@ -9,7 +9,7 @@ module LengthConversions
     miles * 5280 # Cálculo para converte milhas para pés.
   end
 
-  def self.miles.to_inches(miles) # Para indica que o metódo que foi criado, faz parte do meu módulo utilizamos o <self.(nome módulo)>.
+  def self.miles_to_inches(miles) # Para indica que o metódo que foi criado, faz parte do meu módulo utilizamos o <self.(nome módulo)>.
     feet = miles_to_feet(miles) # Como ver, aqui não precisamos do <self.>, já que estamos execultando o módulo em si e não atribuindo. Criando a referência para reaproveitar o módulo <self.miles_to_feet>.
     feet * 12 # Cálculo para converte milhas para polegadas. feet = 5280 * 12 = 63360.
   end
@@ -19,3 +19,7 @@ module LengthConversions
     inches * 2.54 # Cálculo para converte milhas para centímetros. inches = 63280 * 2.54 = 160934.
   end
 end
+
+puts LengthConversions.miles_to_feet(10) # Para fazer a conversão de milhas para pés.
+puts LengthConversions.miles_to_feet(10) # Para fazer a conversão de milhas para polegas.
+puts LengthConversions.miles_to_inches(10) # Para fazer a conversão de milhas para centímetros.
