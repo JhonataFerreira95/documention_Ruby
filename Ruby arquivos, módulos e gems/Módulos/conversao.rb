18288bd93058b0fdbee8e0e2baa4d02a83dd35c1
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-20 16:26:24 -0300

    movido os módulo para pasta módulo e adc o readme da conversão

--- Ruby arquivos, módulos e gems/Módulos/conversao.rb
+++ Ruby arquivos, módulos e gems/Módulos/conversao.rb
@@ -23,3 +23,5 @@ end
 puts LengthConversions.miles_to_feet(10) # Para fazer a conversão de milhas para pés.
 puts LengthConversions.miles_to_inches(20) # Para fazer a conversão de milhas para polegas.
 puts LengthConversions.miles_to_centimers(30) # Para fazer a conversão de milhas para centímetros.
+
+
