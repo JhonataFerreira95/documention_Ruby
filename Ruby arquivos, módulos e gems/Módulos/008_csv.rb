a956ba539c9d5ad11f628ff6a9283658ad7717b0
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-03-26 23:32:17 -0300

    csv 75%

--- Ruby arquivos, módulos e gems/Módulos/008_csv.rb
+++ Ruby arquivos, módulos e gems/Módulos/008_csv.rb
@@ -6,4 +6,10 @@ require 'csv' # Importando o módulo.
 
 CSV.open("../Manipulação de arquivos/Csv/exemplo.csv", "w") do |csv| # Aqui vamos escreve utilizando o <CSV.open>, passamos o <"w"> que significa que vamos escrever, criamos um bloco com <do> o bloco vai ser como parâmetro nossa variável <|csv|>.
     csv << ["Nome", "idade", "cidade"] # Estamos escrevendo no arquivo utilizando o <Binary left_shift>.
+    csv << ["Shademan", "90", "?"]
+    csv << ["Rockman", "22", "NetCity"]
+    csv << ["Protoman" "24", "NetCity"]
+end
+
+
 
