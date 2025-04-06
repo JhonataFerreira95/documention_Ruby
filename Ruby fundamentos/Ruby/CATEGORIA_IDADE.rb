7b8365d43e64eec05c9559bb0f0eeecfd8085810
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-06 03:57:13 -0300

    atualizado a função categoria

--- Ruby fundamentos/Ruby/CATEGORIA_IDADE.rb
+++ Ruby fundamentos/Ruby/CATEGORIA_IDADE.rb
@@ -1,13 +1,19 @@
+
+
+def categoria(idade)
+  if idade < 13
+    puts "Você é uma crinaça, sua idade é #{idade}"
+  elsif idade <= 17
+    puts "Você é um adolescente, sua idade é #{idade}"
+  elsif idade <= 65
+    puts "Você é um adulto, sua idade é #{idade}"
+  elsif idade >= 65
+    puts "Você é idoso, sua idade é #{idade}"
+  end
+end
+
 puts "Digite a sua idade para verificação de categoria: "
 
-idade = gets.chomp.to_i
+age = gets.chomp.to_i
 
-if idade < 13
-  puts "Você é uma crinaça, sua idade é #{idade}"
-elsif idade <= 17
-  puts "Você é um adolescente, sua idade é #{idade}"
-elsif idade <= 65
-  puts "Você é um adulto, sua idade é #{idade}"
-elsif idade >= 65
-  puts "Você é idoso, sua idade é #{idade}"
-end
+puts categoria(age)
