da1d1461c89bb8e9d78e7415663e2607f798dfe6
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-06 01:33:14 -0300

    atualiado a categoria de idade

--- Ruby fundamentos/Ruby/CATEGORIA_IDADE.rb
+++ Ruby fundamentos/Ruby/CATEGORIA_IDADE.rb
@@ -4,10 +4,10 @@ idade = gets.chomp.to_i
 
 if idade < 13
   puts "Você é uma crinaça, sua idade é #{idade}"
-elsif idade >= 17
+elsif idade <= 17
   puts "Você é um adolescente, sua idade é #{idade}"
-elsif idade >= 64 
+elsif idade <= 65
   puts "Você é um adulto, sua idade é #{idade}"
-elsif idade > 65
+elsif idade >= 65
   puts "Você é idoso, sua idade é #{idade}"
 end
