a865b9c8dd6211586c05be059114c39bcd97134a
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-12 18:07:48 -0300

    att

--- Ruby arquivos, módulos e gems/projetos/main.rb
+++ Ruby arquivos, módulos e gems/projetos/main.rb
@@ -11,5 +11,7 @@
  require_relative 'conversao'
 
 puts LengthConversions.miles_to_feet(10) # Para fazer a conversão de milhas para pés.
+
 puts LengthConversions.miles_to_inches(20) # Para fazer a conversão de milhas para polegas.
+
 puts LengthConversions.miles_to_centimers(30) # Para fazer a conversão de milhas para centímetros.
