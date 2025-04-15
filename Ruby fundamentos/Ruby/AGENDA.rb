3420ca3383e42e24a11e35ff077c1e6c5a5ac16a
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-15 00:21:14 -0300

    trabalhando com hash 2/3

--- Ruby fundamentos/Ruby/AGENDA.rb
+++ Ruby fundamentos/Ruby/AGENDA.rb
@@ -1,5 +1,12 @@
-agenda = [:nome => "Anna", :number => 2832-3923]
-agenda = [:nome => "Pedro", :number => 2325-3923]
-agenda = [:nome => "Lucas", :number => 1124-9801]
+agenda = [:nome => "Anna", :number => 2832-3923], [:nome => "Pedro", :number => 2325-3923], [:nome => "Lucas", :number => 1124-9801]
 
-puts "Bem vindo a agenda de contatos com hash!"
+puts "Bem vindo a agenda de contatos com hash! \n----------------------------------------\n"
+
+puts "Qual contato deseja busca, digite o nome do contato: "
+contato = gets.chomp
+
+if contato.has_key?("Anna") or (contato.has_key?("Lucas") or contato.has_key?("Pedro"))
+    puts "O contato foi achado #{agenda}"
+else
+  puts "Seu contato não foi achado, tente novamente!"
+end
