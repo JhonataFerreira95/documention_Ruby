c1370b0e5dff790baa7943e3d0a896f66d66ff80
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-14 23:54:33 -0300

    trabalhando com hash 1/3

---
+++ Ruby fundamentos/Ruby/AGENDA.rb
@@ -0,0 +1,5 @@
+agenda = [:nome => "Anna", :number => 2832-3923]
+agenda = [:nome => "Pedro", :number => 2325-3923]
+agenda = [:nome => "Lucas", :number => 1124-9801]
+
+puts "Bem vindo a agenda de contatos com hash!"
