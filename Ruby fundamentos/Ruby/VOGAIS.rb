6fe5dda6418fb23aa6667c50466739736d0257b1
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-17 19:25:21 -0300

    feito as vogais

--- Ruby fundamentos/Ruby/VOGAIS.rb
+++ Ruby fundamentos/Ruby/VOGAIS.rb
@@ -1,11 +1,18 @@
 puts "Bem vindo ao app de conta vógais! \n---------------------------------\n"
 
 puts "Digite alguma palavra/texto para que possamos contar a quantidade de vógais: "
-vogais = gets.chomp.downcase.strip
+texto = gets.chomp.downcase.strip
 
+contador_vogais = 0
+vogais_encontradas = ""
 
-for i in vogais.length
-  if vogais = 
-    puts "As vógais encontradas #{vogais}"
+for i in 0...texto.length
+  caractere = texto[i]
+  if "aeiouáàâãéèêíìóòôõúù".include?(caractere) 
+    contador_vogais += 1
+    vogais_encontradas += caractere
   end
 end
+
+puts "Foram encontradas #{contador_vogais} vogais no texto."
+puts "As vogais encontradas foram: #{vogais_encontradas}"
