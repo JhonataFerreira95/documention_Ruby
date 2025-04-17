41f0a39951459d88e3cf50bd04249652dce9dd1e
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-17 16:04:21 -0300

    ainda não finalizado o vogais

---
+++ Ruby fundamentos/Ruby/VOGAIS.rb
@@ -0,0 +1,11 @@
+puts "Bem vindo ao app de conta vógais! \n---------------------------------\n"
+
+puts "Digite alguma palavra/texto para que possamos contar a quantidade de vógais: "
+vogais = gets.chomp.downcase.strip
+
+
+for i in vogais.length
+  if vogais = 
+    puts "As vógais encontradas #{vogais}"
+  end
+end
