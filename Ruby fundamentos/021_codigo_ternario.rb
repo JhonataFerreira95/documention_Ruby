f28996ca117f6a1f0ebc163826c3135ec63c126b
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-27 18:35:45 -0300

    att

---
+++ Ruby fundamentos/021_codigo_ternario.rb
@@ -0,0 +1,14 @@
+# O que é um código ternário? É um código mais limpo e refatorado.
+
+# Exemplo sem código ternário:
+
+nome = "Kuma"
+
+if nome == "kuma"
+    puts "Kuma é um fofo"
+else
+  puts "Kuma não é um fofo"
+end
+
+# Exemplos com código ternário:
+
