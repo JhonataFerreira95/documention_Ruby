60f740f620a54c791369f2450ff7c4bba19aae38
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-27 22:33:30 -0300

    codigo ternario

--- Ruby fundamentos/021_codigo_ternario.rb
+++ Ruby fundamentos/021_codigo_ternario.rb
@@ -1,14 +1,17 @@
-# O que é um código ternário? É um código mais limpo e refatorado.
+# O que é um código ternário? É um código mais limpo e refatorado. 
+# Um operador ternário é composto por três partes, daí vem a palavra "ternário". Essas partes incluem uma declaração condicional e dois resultados possíveis.
 
 # Exemplo sem código ternário:
 
-nome = "Kuma"
+nome = "kuma"
 
 if nome == "kuma"
-    puts "Kuma é um fofo"
+  puts "Kuma é um fofo"
 else
   puts "Kuma não é um fofo"
 end
 
 # Exemplos com código ternário:
+
+puts nome.eql("kuma")? "Kuma é um fofo" : "kuma não é fofo" # Aqui utilizamos o <.eql>  que verificar se os objetos são equivalentes.
 
