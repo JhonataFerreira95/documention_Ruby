9fc131ed162aec1ea1bd60e3284f3dd75599eec6
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-30 16:35:02 -0300

    feito o jogo jokenpo hxh

--- Ruby fundamentos/Ruby/JOKENPO.rb
+++ Ruby fundamentos/Ruby/JOKENPO.rb
@@ -1,4 +1,27 @@
-require "random"
+require "securerandom"
+
+itens = ["Pedra", "Papel", "Tesoura"]
+
+computer =  SecureRandom.random_number(0..2)
+
+puts "Sua opções são\n-=-=-=-=-=-=-=-=-\n[0]Pedra\n-----------------\n[1]Papel\n-----------------\n[2]Tesoura\n-=-=-=-=-=-=-=-=-\n"
+
+puts "Qual é a sua jogada?"
+jogador = gets.chomp.to_i
+
+if !(0..2).include?(jogador) # Para negar qualquer entrada que não seja entre 0 e 2.
+  puts "JOGADA INVÁLIDA! \n-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=\nTente novamente com as opções apresentadas\n-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=\n"
+elsif computer == jogador
+  puts "Empatou, o computador jogou #{itens[computer]} e o jagador jogou #{itens[jogador]}"
+elsif computer > jogador
+  puts "O computador venceu! O computador jogou #{itens[computer]} e o jogador jogou #{itens[jogador]}"
+elsif jogador > computer
+  puts "O jogador venceu! O jogadoor jogou #{itens[jogador]} e o computador jogou #{itens[computer]}"
+end
+
+puts ("-=" * 35)
+
+
 
 
 
