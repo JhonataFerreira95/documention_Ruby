2ee1c84cf08202296f744f53d3d6891b50f4be71
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-05-24 12:48:41 -0300

    Feito o validar dados sobre S/M

--- Ruby fundamentos/Ruby/VALIDAR_DADOS.rb
+++ Ruby fundamentos/Ruby/VALIDAR_DADOS.rb
@@ -5,9 +5,12 @@ puts ("=-=-=" * 8)
 puts "Digite o seu sexo [M/F]: "
 sexo = gets.chomp.upcase
 
-while sexo
-  
+while "ABCDEGHIJKLOPQRSTUVXYWZ".include?(sexo)
+    puts "Digite um sexo válido [M/F]: "
+    sexo = gets.chomp.upcase
 end
+
+puts "O seu sexo é #{sexo}!"
 
 puts ("=-=-=" * 8)
 
