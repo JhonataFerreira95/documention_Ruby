a7b979f720bf87406b4ee84c91653c8e0459a372
Jhonata Ferreira <87663377+JhonataFerreira95@users.noreply.github.com>
2025-07-17 08:17:28 -0300

    Update GAME_PAR_IMPA.rb

--- Ruby fundamentos/Ruby/GAME_PAR_IMPA.rb
+++ Ruby fundamentos/Ruby/GAME_PAR_IMPA.rb
@@ -1,17 +1,29 @@
 require 'securerandom' 
 
-jogador = par = impa = pc = 0
+ jogador = par = impa = pc = 0
+ opcao = ""
+ opcaoPc = ""
 
 puts "Bem vindo ao game de par ou ímpar!"
 
-pc = SecureRandom.random_number(0..1000)
 
 while true 
 
-  puts "=-=-="*8
-  puts "Escolha entre par ou ímpar[I/P]: "
-  puts "=-=-="*8
-  jogador = gets.chomp.to_i
+  pc = SecureRandom.random_number(0..1000)
 
+  puts "=-=-="*8
+  puts "Escolha entre par ou ímpar[I/P]: ".upcase
+  opcao = gets.chomp
+  puts "=-=-="*8
+  
+  if opcao.include?("I")
+
+    puts "Digite um número para jogar com o computador: "
+    jogador =  gets.chomp.to_i
+  
+  end
+
+  opcaoPc != opcao
+  puts opcaoPc
 
 end
