28bd81db320ee7b9e44602e638dc70dd64b77bb2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-07-17 21:37:39 -0300

    não finalizado

--- Ruby fundamentos/Ruby/GAME_PAR_IMPA.rb
+++ Ruby fundamentos/Ruby/GAME_PAR_IMPA.rb
@@ -1,6 +1,6 @@
 require 'securerandom' 
 
- jogador = par = impa = pc = 0
+ cont = jogador = par = impa = pc = 0
  opcao = ""
  opcaoPc = ""
 
@@ -9,21 +9,48 @@ puts "Bem vindo ao game de par ou ímpar!"
 
 while true 
 
-  pc = SecureRandom.random_number(0..1000)
+  pc = SecureRandom.random_number(0..100000)
 
   puts "=-=-="*8
-  puts "Escolha entre par ou ímpar[I/P]: ".upcase
-  opcao = gets.chomp
+  puts "Escolha entre par ou ímpar[I/P]: "
+  opcao = gets.chomp.upcase
   puts "=-=-="*8
   
-  if opcao.include?("I")
+  if opcao.include?("P")
+
+    puts "=-=-="*8
+    puts "Você escolheu ímpar"
+    puts "=-=-="*8
 
     puts "Digite um número para jogar com o computador: "
     jogador =  gets.chomp.to_i
-  
+
+    if jogador % 2 == 0 and pc % 2 == 1
+
+      sleep 0.3
+      puts "O computador jogou ímpar #{pc}\n Você jogou par #{jogador}"
+
+    end
+
+  elsif opcao.include?("I")
+
+    puts "=-=-="*8
+    puts "Você escolheu par"
+    puts "=-=-="*8
+
+    puts "Digite um número para jogar com o computador: "
+    jogador =  gets.chomp.to_i
+
+    if jogador % 2 == 1 and pc % 2 == 0
+      
+      sleep 0.3
+      puts "O computador jogou par #{pc}\n Você jogou ímpar #{jogador}"
+
+    end
+
   end
 
-  opcaoPc != opcao
-  puts opcaoPc
+  cont += 1
+  puts cont
 
 end
