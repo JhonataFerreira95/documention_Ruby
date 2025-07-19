ce145965ae859ee19300477124c2b645f3e6b800
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-07-18 23:31:50 -0300

    leetcode incompleto

---
+++ Ruby fundamentos/Ruby/IDADE_SEXO_VARIADO.rb
@@ -0,0 +1,26 @@
+idade = 0
+sexo = ""
+cadastro = ""
+
+while true
+
+  puts "-=-=-"*8
+  puts "[CADASTRE UMA PESSOA]"
+  puts "-=-=-"*8
+
+  puts  "Idade: "
+  idade = gets.chomp.to_i
+  puts "Sexo [M/F]: "
+  sexo = gets.chomp.upcase
+
+  puts "-=-=-"*8
+  puts "Quer continua?[S/N] "
+  cadastro = gets.chomp.upcase
+  puts "-=-=-"*8
+
+  if cadastro.include?("N")
+      puts ""
+      break
+  end
+
+end
