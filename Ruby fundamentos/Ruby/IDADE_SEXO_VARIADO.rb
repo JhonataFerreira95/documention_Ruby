c0fa81faf5ccb310200b60877b3b7faef0f86ea6
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-08-22 11:54:55 -0300

    leetcode feito

--- Ruby fundamentos/Ruby/IDADE_SEXO_VARIADO.rb
+++ Ruby fundamentos/Ruby/IDADE_SEXO_VARIADO.rb
@@ -1,3 +1,4 @@
+total = totalM = totalF20 =0 
 
 while true
 
@@ -9,6 +10,18 @@ while true
   while sexo != "M" && sexo != "F"
     puts "Digite o sexo [M/F]: "
     sexo = gets.chomp.upcase
+  end
+
+  if idade >= 18
+    total += 1
+  end
+
+  if sexo == "M"
+    totalM += 1
+  end
+
+  if sexo == "F" and idade < 20
+    totalF20 += 1
   end
 
   resp = " "
@@ -23,3 +36,9 @@ while true
     end
 
 end
+
+puts "Total de pessoas com mais de 18 anos: #{total}"
+puts "=-=-=-=" * 8
+puts "Ao todo temos #{totalM} homens cadastrado"
+puts "=-=-=-=" * 8
+puts "E temos #{totalF20} mulheres com menos de 20 anos"
