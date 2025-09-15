791bfced1b830c12228f3b806d7a62ed196fb95f
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-09-15 20:01:53 -0300

    finalizado os metodo initialize

--- Ruby poo/009_parametros_initialize.rb
+++ Ruby poo/009_parametros_initialize.rb
@@ -4,13 +4,20 @@ class Game
   attr_accessor :name, :genre, :price # leitura e escrita
   attr_reader :multiplayer, :versions # apenas leitura
 
-  def initialize(name, genre, multiplayer, versions, price) # Passando parâmetros no métodos <initialize>
+  def initialize(name, genre, multiplayer, versions, price) # Passando parâmetros no métodos <initialize> para deixá-lo mais dinâmico
       # Variável de instância
-      @name = "pes"
-      @genre = "esporte"
-      @multiplayer = true
-      @versions = [2018, 2019, 2020]
-      @price = 100
+      @name = name
+      @genre = genre
+      @multiplayer = multiplayer
+      @versions = versions
+      @price = price
+  end
+
+  def techinal_sheet
+    puts "Dados do jogo"
+    puts "nome do jogo: #{@name}"
+    puts "genero do jogo: #{@genre}"
+    puts "preço do jogo: #{@price}"
   end
 
   def to_s # método de sobrescrita 
@@ -18,6 +25,22 @@ class Game
   end
 
 end
+
+game1 = Game.new("fifa", "esporte", true, [2018, 2019, 2020], 100) # insirindo valores na ordem de definição dos parâmetros do método <initialize>
+
+puts game1
+
+puts game1.techinal_sheet
+
+game2 = Game.new("hollow knight", "aventura", true, [2017, 2018, 2019, 2020, 2021], 39.99)
+
+puts game2
+
+puts game2.techinal_sheet
+
+
+
+
 
 
 
