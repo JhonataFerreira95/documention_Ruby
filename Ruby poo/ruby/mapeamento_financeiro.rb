e818762250e28996f2cd22535435ed30b8229de2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-09-17 20:14:19 -0300

    mapeamento qause pronto

--- Ruby poo/ruby/mapeamento_financeiro.rb
+++ Ruby poo/ruby/mapeamento_financeiro.rb
@@ -1,7 +1,14 @@
-class FinalTransecion
+class FinancialTransaction
 
-  def initialize
-    
+  attr_accessor: :completed # Acessor tem a mesma função do getter e setter.
+
+  attr_reader: :to, :from, :amount # definição do getter com o <attr_reade> apenas leitura. 
+
+  def initialize(to, from, amount, completed)
+    @to = to
+    @from = from
+    @amount = amount
+    @completed = completed
   end
 
 end
