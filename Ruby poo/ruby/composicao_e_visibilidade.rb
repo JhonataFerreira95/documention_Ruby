4cee786f04112c5aef1b91e00a5c7347e239b8a6
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-10-23 17:59:03 -0300

    compisição issue

---
+++ Ruby poo/ruby/composicao_e_visibilidade.rb
@@ -0,0 +1,42 @@
+class Motor
+
+  attr_accessor :potencia
+
+  def initialize(potencia)
+    @potencia = potencia
+  end
+
+  def to_s
+    "#{@potencia}-"
+  end
+
+  protected 
+
+  def ligar
+    puts "O motor de #{@potencia}CV está ligado!"
+  end
+
+end
+
+class Carro < Motor
+
+  attr_accessor :modelo 
+
+  def initialize(potencia, modelo)
+    super(potencia)
+    @modelo = modelo
+  end
+
+  def to_s
+    "#{super}-#{@modelo}"
+  end
+
+  def iniciar
+    puts "Iniciando o carro #{@modelo}"
+    self.ligar
+  end
+
+end
+
+carro = Carro.new(300, "Civic")
+puts carro.iniciar
