0ce566fb761c74322a12f1511472ca6fdc49ad22
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-11-02 14:24:18 -0300

    adc o ruby lsp

--- Ruby poo/ruby/compartilhar_comportamento.rb
+++ Ruby poo/ruby/compartilhar_comportamento.rb
@@ -1,4 +1,13 @@
+require 'modulo'
+
 class Pessoa
+
+  attr_accessor falar:
+
+  def initialize(falar)
+    include Falante
+    @falar = falar
+  end
 
 end
 
---
+++ Ruby poo/ruby/modulo.rb
@@ -0,0 +1,11 @@
+module Falante
+
+  NOMESISTEMA = "Sistema de fala"
+  VERSAO = 1.2
+
+  def falar(frase)
+    puts "#{frase}"
+  end
+
+end
+
