2c75943e09a7f61cf2c7e710e5963f601aa51012
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-18 22:52:05 -0300

    finalizado

--- Ruby fundamentos/Ruby/MAIOR_MENOR_VALORES_lISTAR.rb
+++ Ruby fundamentos/Ruby/MAIOR_MENOR_VALORES_lISTAR.rb
@@ -1,16 +1,20 @@
 resposta = []
 maior_valor = 0
-menor_valor = 0 
+menor_valor = 0
+maior_posicao = 0
+menor_posicao = 0
 
 for i in 0..4
   puts "Digite a os valores: "
   resposta << gets.chomp.to_i
-  if resposta[i] >= 0
+  if resposta[i] >= maior_valor
     maior_valor = resposta[i]
-  elsif resposta[i] < maior_valor
-    menor_valor = resposta[i]
+    maior_posicao = i
   end
-  puts maior_valor, menor_valor, i
+  if resposta[i] < maior_valor
+    menor_valor = resposta[i]
+    menor_posicao = i
+  end
 end
 
-print "O maior valor da lista é #{resposta.max} o índice do maior valor é #{maior_valor}, e o menor valor da lista é #{resposta.min} o índice do menor valor é #{menor_valor}"
+print "O maior valor da lista é #{resposta.max} o índice do maior valor é #{maior_posicao}, e o menor valor da lista é #{resposta.min} o índice do menor valor é #{menor_posicao}"
