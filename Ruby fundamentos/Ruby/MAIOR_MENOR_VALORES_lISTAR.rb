fef48ea6262f280d5f3033d7bd320ee557e0f6b5
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-18 22:09:18 -0300

    tentativa de mostra o indice de max and min

--- Ruby fundamentos/Ruby/MAIOR_MENOR_VALORES_lISTAR.rb
+++ Ruby fundamentos/Ruby/MAIOR_MENOR_VALORES_lISTAR.rb
@@ -1,10 +1,16 @@
 resposta = []
+maior_valor = 0
+menor_valor = 0 
 
-for i in 1..5
+for i in 0..4
   puts "Digite a os valores: "
   resposta << gets.chomp.to_i
+  if resposta[i] >= 0
+    maior_valor = resposta[i]
+  elsif maior_valor < resposta[i]
+    maior_valor = menor_valor
+  end
+  puts maior_valor, menor_valor
 end
 
-puts maior_valor, menor_valor
-
-print "O maior valor da lista é #{resposta.max}, e o maior valor da lista é #{resposta.min}"
+print "O maior valor da lista é #{resposta.max}, e o menor valor da lista é #{resposta.min}"
