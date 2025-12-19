9ec3850908b7b0e78e0276195e54330e258efdd1
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-19 13:07:21 -0300

    finalizado

--- Ruby fundamentos/Ruby/VALORES_UNICOS_EM_LISTA.rb
+++ Ruby fundamentos/Ruby/VALORES_UNICOS_EM_LISTA.rb
@@ -1,16 +1,19 @@
 valores = []
 count = 0
 
-while true
+while count = 0
+  count += 1
   puts "Digite um valor: "
   valores << gets.chomp.to_i
-  count += 1
+  if valores[count] == valores[count]
+    valores.delete_at(count)
+  end
+  puts "o valor digitado #{valores} seu índice #{count}"
   puts "Para sair do programa digite[S]: "
   sair = gets.chomp.upcase
   if sair.include?("S")
     break
   end
-  puts valores, count
 end
 
 puts "Todos os valores únicos digitados foram #{valores.sort!}"
