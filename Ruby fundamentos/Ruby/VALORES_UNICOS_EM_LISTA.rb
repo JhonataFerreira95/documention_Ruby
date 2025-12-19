e01a0ae8eb4a3280675275fc2c969a55320e2f79
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-19 03:03:40 -0300

    falta apenas a lógica dos número não se repetir

--- Ruby fundamentos/Ruby/VALORES_UNICOS_EM_LISTA.rb
+++ Ruby fundamentos/Ruby/VALORES_UNICOS_EM_LISTA.rb
@@ -5,8 +5,10 @@ while true
   puts "Digite um valor: "
   valores << gets.chomp.to_i
   count += 1
-  if valores == valores
-        
+  if valores.include?(20)
+    break
   end
   puts valores, count
 end
+
+puts "Todos os valores únicos digitados foram #{valores.sort!}"
