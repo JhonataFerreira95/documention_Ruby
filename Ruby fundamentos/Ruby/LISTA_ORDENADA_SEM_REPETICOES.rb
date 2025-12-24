d873d827cc4f505bf73a911ef4f9aaf7055c0e8c
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-23 22:37:23 -0300

    outra tentativa

--- Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
+++ Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
@@ -3,22 +3,10 @@ lista = []
 5.times do |i|
   puts "Digite o valor para ser adicionado a lista: "
   lista << gets.chomp.to_i
-  case lista
-    when lista[i] == 1
-      puts lista
-    when lista[i] == 2
-      puts lista
-    when lista[i] == 3
-      puts lista
-    when lista[i] == 4
-      puts lista 
-    when lista[i] == 5
-      puts lista
+  if lista[i] <= i
+    lista.insert(0, i)
+    puts lista
   end
 end
 
 puts "A posiçao dos número em sua lista foram #{lista}"
-
-if lista[1] == 1
-    
-end
