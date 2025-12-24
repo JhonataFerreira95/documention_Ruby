491b6d17b2322ddf4508d9b5ba4691c7319cebdf
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-24 00:13:56 -0300

    removi a lógica de dentro do laço time

--- Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
+++ Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
@@ -3,10 +3,11 @@ lista = []
 5.times do |i|
   puts "Digite o valor para ser adicionado a lista: "
   lista << gets.chomp.to_i
-  if lista[i] <= i or lista[i] >= i
-    if lista[i] == i
+end
+
+if lista[i] <= i or lista[i] >= i
+  if lista[i] == i
     end
-  end
 end
 
 puts "A posiçao dos número em sua lista foram #{lista}"
