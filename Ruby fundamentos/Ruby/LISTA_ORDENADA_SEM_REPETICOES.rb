b6ae768d57e600ad83d4ab22843bacbbb17077c6
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-24 22:52:27 -0300

    att

--- Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
+++ Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
@@ -3,11 +3,10 @@ lista = []
 5.times do |i|
   puts "Digite o valor para ser adicionado a lista: "
   lista.insert(-1, gets.chomp.to_i)
-  if lista[4] > i
+  if i == 0
     lista[4] = i
-  end
-  if lista[3] <= i
-    lista[2] = i
+  elsif i > lista.length - 1
+    puts "oi"
   end
 end
 
