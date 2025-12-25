00027c4080e6e4e3fb3b386b1d56e20d8186a6c2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-24 22:58:59 -0300

    lista.inspect

--- Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
+++ Ruby fundamentos/Ruby/LISTA_ORDENADA_SEM_REPETICOES.rb
@@ -1,14 +1,16 @@
 lista = []
 
 5.times do |i|
-  puts "Digite o valor para ser adicionado a lista: "
-  lista.insert(-1, gets.chomp.to_i)
-  if i == 0
-    lista[4] = i
-  elsif i > lista.length - 1
-    puts "oi"
+
+  puts "Digite um valor:"
+  valor = gets.chomp.to_i
+
+  posicao = 0
+  while posicao < lista.length && lista[posicao] < valor
+    posicao += 1
   end
+
 end
 
-puts "A posiçao dos número em sua lista foram #{lista}"
+puts "A posiçao dos número em sua lista foram #{lista.inspect}"
 
