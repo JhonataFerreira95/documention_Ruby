97ddd9f9a94f31a6695cec0b13da8ce7d7a9a750
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-25 12:29:10 -0300

    esborço

--- Ruby fundamentos/Ruby/EXTRAINDO_DADOS_EM_LISTA.rb
+++ Ruby fundamentos/Ruby/EXTRAINDO_DADOS_EM_LISTA.rb
@@ -2,5 +2,16 @@ count = 0
 lista =[]
 
 while true
+  puts "Digite um valor que vai ser adicionado na lista: "
+  lista << gets.chomp.to_i
+
+  count += 1
+
+  puts "Deseja continuar adicionando números na lista [S/N]: "
+  decisao = gets.chomp.to_i.upcase
+
+  if decisao.include?("N")
+    break
+  end
   
 end
