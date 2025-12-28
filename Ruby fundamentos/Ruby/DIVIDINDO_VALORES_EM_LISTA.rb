ac1cfdfcfce44b791b5ee491b088383fc73cede5
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-12-28 18:34:05 -0300

    quase finalizando

--- Ruby fundamentos/Ruby/DIVIDINDO_VALORES_EM_LISTA.rb
+++ Ruby fundamentos/Ruby/DIVIDINDO_VALORES_EM_LISTA.rb
@@ -1,8 +1,14 @@
-lista = []
+lista = impar = par = []
+count = 0
+
 
 while true
   puts "Digite o valor Desejado: "
   lista << gets.chomp.to_i
+  count += 1
+  if lista[count] % 2 == 0
+    puts "oi" 
+  end
   puts "Deseja continuar[S/N]: "
   sair = gets.chomp.upcase
   if sair.include?("N")
