b3e663ddbee1ff2c6b869578383c1c4dcebfc7fc
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-21 00:14:32 -0300

    testando

--- Ruby fundamentos/Ruby/VALIDANDO_EXPRESSÕES_MATEMATICAS.rb
+++ Ruby fundamentos/Ruby/VALIDANDO_EXPRESSÕES_MATEMATICAS.rb
@@ -1,7 +1,13 @@
 
 puts "Digite a expressão matemática para ver se é válida: "
-expressao = gets.chomp
+expressao << gets.chomp
 pilha = []
 
-
+for simb in expressao
+    if simb == ("(")
+      pilha.push("(")
+    elsif simb == (")")
+      pilha.push(")")
+    end
+end
 
