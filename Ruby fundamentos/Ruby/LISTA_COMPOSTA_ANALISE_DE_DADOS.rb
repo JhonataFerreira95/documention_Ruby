8967ecc0c9bbde27784dbdeeec60b956b8bf5eb6
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-24 23:16:11 -0300

    feito a base

--- Ruby fundamentos/Ruby/LISTA_COMPOSTA_ANALISE_DE_DADOS.rb
+++ Ruby fundamentos/Ruby/LISTA_COMPOSTA_ANALISE_DE_DADOS.rb
@@ -2,6 +2,14 @@ quantas = []
 pesadas = []
 leves = []
 
+count = 0
+
 while true
-      
+    puts "Digite o seu nome: "
+    nome = gets.chomp
+    puts "Digite o sue peso em KG: "
+    peso = gets.chomp.to_f
+
+    count += 1
+    
 end
