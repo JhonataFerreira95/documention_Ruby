0a4c5981ab484534103040ca6448c127338867fc
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-27 16:55:58 -0300

    novo problema

--- Ruby fundamentos/Ruby/LISTA_COMPOSTA_ANALISE_DE_DADOS.rb
+++ Ruby fundamentos/Ruby/LISTA_COMPOSTA_ANALISE_DE_DADOS.rb
@@ -1,7 +1,9 @@
 dados = []
 count = 0
 mais_pesados = 0
+nome_mais_pesado = ''
 mais_leves = 0
+nome_mais_leve = ''
 
 while true
 
@@ -13,6 +15,14 @@ while true
     dados = [nome, peso]
 
     count += 1
+
+    if peso >= 0
+        peso = mais_pesados
+        nome = nome_mais_pesado
+    elsif peso <= mais_pesados
+        peso = mais_leves
+        nome = nome_mais_leve
+    end
 
     puts "Desejar continuar[S/N]: "
     sair = gets.chomp.upcase
