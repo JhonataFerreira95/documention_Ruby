f7957792dc4755a295af01ee64f36c57d497f2a6
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-28 16:10:57 -0300

    quase finalizdo

--- Ruby fundamentos/Ruby/LISTA_COM_PARES_IMPARES_ORDEM.rb
+++ Ruby fundamentos/Ruby/LISTA_COM_PARES_IMPARES_ORDEM.rb
@@ -4,7 +4,15 @@ impares = []
 count = 0 
 
 while count < 7
+
   count += 1
   puts "Digite o valor #{count}"
   valor = gets.chomp.to_i
+
+  if valor % 2 
+    pares << valor
+  else
+    impares << impares
+  end
+  
 end
