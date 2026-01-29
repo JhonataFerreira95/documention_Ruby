fc8d2ada945fc949115fb2f30850c6e2888cd9aa
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-01-29 16:21:39 -0300

    alocando

--- Ruby fundamentos/Ruby/LISTA_COMPOSTA_ANALISE_DE_DADOS.rb
+++ Ruby fundamentos/Ruby/LISTA_COMPOSTA_ANALISE_DE_DADOS.rb
@@ -10,12 +10,18 @@ while true
     puts "Digite o sue peso em KG: "
     temp << gets.chomp.to_i
 
+    if dados.size == 0
+        maior = menor = temp[1]
+    else
+        if temp[1] > maior
+            maior = temp[1]
+        end
+    end
+
     dados.push(temp.dup)
     temp.clear
 
     count += 1
-
-    
 
     puts "Desejar continuar[S/N]: "
     sair = gets.chomp.upcase
