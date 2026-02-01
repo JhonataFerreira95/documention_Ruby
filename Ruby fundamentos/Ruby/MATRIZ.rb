6765e100afebbc0f4880af6113332a0cadb9fcf1
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-02-01 17:43:02 -0300

    formatado

--- Ruby fundamentos/Ruby/MATRIZ.rb
+++ Ruby fundamentos/Ruby/MATRIZ.rb
@@ -27,5 +27,7 @@ puts "-=" * 10
 puts "A soma dos valores pares é #{spar}"
 
 for l in 0..2
-    scol += matriz[l][2]
+  scol += matriz[l][2]
 end
+
+puts "A soma dos valores da coluna 3 são #{scol}"
