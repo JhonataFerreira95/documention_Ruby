1331d0ebbe8bbfaf0d229979fe5aba92333d4844
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-02-05 17:23:46 -0300

    feito a lógica inicial

--- Ruby fundamentos/Ruby/PALPITES_MEGA_SENA.rb
+++ Ruby fundamentos/Ruby/PALPITES_MEGA_SENA.rb
@@ -3,6 +3,10 @@ require 'securerandom'
 sorteio = []
 count = 0
 
-while count 
-    
+puts "Quantos jogos você quer que eu sorteie? "
+palpites = gets.chomp.to_i
+
+while count <= palpites 
+  puts "algo #{count}" 
+  count += 1
 end
