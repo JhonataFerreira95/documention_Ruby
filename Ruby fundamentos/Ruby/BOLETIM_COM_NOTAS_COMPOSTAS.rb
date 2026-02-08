1408d153d44621c2fd1710c75349d740bd9dda10
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-02-08 08:29:10 -0300

    adc novas camadas

--- Ruby fundamentos/Ruby/BOLETIM_COM_NOTAS_COMPOSTAS.rb
+++ Ruby fundamentos/Ruby/BOLETIM_COM_NOTAS_COMPOSTAS.rb
@@ -5,4 +5,14 @@ while true
     puts "Digite o nome do aluno: "
     dados << gets.chomp
     puts "Digite a nome do aluno: "
+    dados << gets.chomp.to_f
+
+    count += 1
+
+    puts "Deseja continuar[S/N]: ".upcase
+    resposta = gets.chomp
+
+    if resposta.include?("N")
+        break
+    end
 end
