8cb12ff651f3b795c9f99b829f5c1e6eb50d4273
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-02-21 10:21:55 -0300

    feito a lógica de pegar as notas tira a média e lista composta

--- Ruby fundamentos/Ruby/BOLETIM_COM_NOTAS_COMPOSTAS.rb
+++ Ruby fundamentos/Ruby/BOLETIM_COM_NOTAS_COMPOSTAS.rb
@@ -7,13 +7,19 @@ while true
 
     puts "Digite o nome do aluno: "
     nome = gets.chomp
-    puts "Digite a #{nota}1 do aluno: "
-    nota1 = gets.chomp
-    puts "Digite a #{nota}2 do aluno: "
-    nota2 = gets.chomp
+    puts "Digite a 1 do aluno: "
+    nota1 = gets.chomp.to_f
+    puts "Digite a 2 do aluno: "
+    nota2 = gets.chomp.to_f
 
-    puts "Deseja continuar[S/N]: ".upcase
-    resposta = gets.chomp
+    media = (nota1 + nota2) / 2
+
+    ficha.push([nome, [nota1, nota2], media])
+
+    puts ficha
+
+    puts "Deseja continuar[S/N]: "
+    resposta = gets.chomp.upcase
 
     if resposta.include?("N")
         break
