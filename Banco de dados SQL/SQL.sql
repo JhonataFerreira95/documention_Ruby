0bec4662aa534fc5b79ed92960e9eb03532ff0b1
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-07 01:21:54 -0300

    feito o backup

---
+++ Banco de dados SQL/SQL.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict FrRcvrLrxRcYtRL1xXD5bvxroR8dxcT2I6KczhfnBc8geUmVufppOOz4d7x6uvm
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+-- Started on 2026-04-07 01:17:54
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-04-07 01:17:56
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict FrRcvrLrxRcYtRL1xXD5bvxroR8dxcT2I6KczhfnBc8geUmVufppOOz4d7x6uvm
+
