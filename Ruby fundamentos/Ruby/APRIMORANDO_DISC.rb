331054a7af142213f72f085ea37c6ec66f5e0e8e
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-19 22:30:49 -0300

    mudei o nome do arquivo

--- Ruby fundamentos/Ruby/APRIMORANDO_DISC.rb
+++
@@ -1,4 +0,0 @@
-hash = {nome: 'joao', idade: 20}
-
-puts hash[:nome] 
-puts hash[:idade]
---
+++ Ruby fundamentos/Ruby/APRIMORANDO_HASH.rb
@@ -0,0 +1,9 @@
+locadora = [
+  {nome: 'Bass', idade: 24},
+  {nome: 'gg', idade:'?'},
+  {nome: 'pragmata', idade: 22}
+]
+
+for k, v in locadora.to_a
+    puts k, v
+end
