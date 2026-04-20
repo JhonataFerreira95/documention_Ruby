e279dda6a983539967a9351a539bd76c05cae083
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-20 12:03:16 -0300

    primeira tentativa

--- Ruby fundamentos/Ruby/APRIMORANDO_HASH.rb
+++ Ruby fundamentos/Ruby/APRIMORANDO_HASH.rb
@@ -1,9 +1,29 @@
-locadora = [
-  {nome: 'Bass', idade: 24},
-  {nome: 'gg', idade:'?'},
-  {nome: 'pragmata', idade: 22}
-]
 
-for k, v in locadora.to_a
-    puts k, v
+cont = media = 0
+
+while true
+
+    puts "Digite o seu nome: "
+    nome = gets.chomp
+
+    puts "Quantidade de bimestre/semestre que possui: "
+    quant = gets.chomp.to_i
+
+    while quant > 0
+      cont += 1
+      puts "Digite a sua #{cont} nota: " 
+      notas = gets.chomp.to_i
+      
+      if cont >= quant
+        puts "Saindo..."
+        media = soma_notas / 2
+
+        puts media
+        break
+      end
+
+    end
+
+
+
 end
