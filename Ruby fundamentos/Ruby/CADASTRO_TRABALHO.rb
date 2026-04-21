66d17e82e0408455baeee9726cda484cac22b3b2
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-21 17:19:16 -0300

    ajustado

--- Ruby fundamentos/Ruby/CADASTRO_TRABALHO.rb
+++ Ruby fundamentos/Ruby/CADASTRO_TRABALHO.rb
@@ -4,20 +4,24 @@ dados = {}
 puts "Digite o seu nome: "
 dados[:nome] = gets.chomp
 
-puts "Digite a sua anos de senhor(a) #{dados[:nome]}: "
+puts "Digite a sua idade senhor(a) #{dados[:nome]}: "
 dados[:idade] = gets.chomp.to_i
 
-puts "Digite a data do seus nascimetno: "
+puts "Digite a o ano do seu nascimento: "
 dados[:nascimetno] = gets.chomp.to_i
 
 puts "Digite o seu CTPS: "
 dados[:ctps] = gets.chomp.to_i
+
+puts dados
 
 if dados[:ctps] != 0
   puts "Digite o primeiro ano da sua contratação: "
   dados[:contratacao] = gets.chomp.to_i
   puts "Digite o seu salários: "
   dados[:salario] = gets.chomp.to_i
+else
+  puts "Tente novamente com um CTPS válido!"
 end
 
 
