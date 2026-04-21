99477b2a1dac4d09fe3cc5a840a02f378e357200
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-21 17:13:49 -0300

    feito a coleta se o CTPS for diferente de 0

--- Ruby fundamentos/Ruby/CADASTRO_TRABALHO.rb
+++ Ruby fundamentos/Ruby/CADASTRO_TRABALHO.rb
@@ -8,12 +8,19 @@ puts "Digite a sua anos de senhor(a) #{dados[:nome]}: "
 dados[:idade] = gets.chomp.to_i
 
 puts "Digite a data do seus nascimetno: "
-dados[:nascimetno] = gets.chomp
+dados[:nascimetno] = gets.chomp.to_i
 
 puts "Digite o seu CTPS: "
 dados[:ctps] = gets.chomp.to_i
 
 if dados[:ctps] != 0
-    
+  puts "Digite o primeiro ano da sua contratação: "
+  dados[:contratacao] = gets.chomp.to_i
+  puts "Digite o seu salários: "
+  dados[:salario] = gets.chomp.to_i
 end
+
+
+
+
 
