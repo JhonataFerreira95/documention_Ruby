a3bb420ce70e9ea79bc990f6efa52cda0e317497
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-21 16:48:58 -0300

    tentando de outro jeito

--- Ruby fundamentos/Ruby/CADASTRO_TRABALHO.rb
+++ Ruby fundamentos/Ruby/CADASTRO_TRABALHO.rb
@@ -4,4 +4,11 @@ dados = {}
 puts "Digite o seu nome: "
 dados[:nome] = gets.chomp
 
-puts "Digite a sua idade senhor(a) #{dados[:nome]}: "
+puts "Digite a sua anos de senhor(a) #{dados[:nome]}: "
+dados[:idade] = gets.chomp.to_i
+
+puts "Digite a data do seus nascimetno: "
+dados[:nascimetno] = gets.chomp
+
+
+
