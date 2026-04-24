6104a954f0a5242a6eaa009c2158db0f0c80e559
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-23 22:02:04 -0300

    finalizado

--- Ruby fundamentos/Ruby/UNINDO_HAHS_E_ARRAYS.rb
+++ Ruby fundamentos/Ruby/UNINDO_HAHS_E_ARRAYS.rb
@@ -6,52 +6,44 @@ No final, mostre: A) Quantas pessoas foram cadastradas B) A média de idade C) U
 =end
 
 dados = []
+soma_idades = 0
 
-cont = 1
+puts "Quantas pessoas quer cadastrar? "
+quantidade = gets.chomp.to_i
 
-idade_media = 0
-acima_da_media = 0
+quantidade.times do
+  cadastro = {}
 
-puts "Quntas pessoas quer cadastrar? "
-quantidade_cadastro = gets.chomp.to_i
+  puts "Digite o nome: "
+  cadastro[:nome] = gets.chomp
 
-while quantidade_cadastro >= cont
+  puts "Digite o sexo [M/F]: "
+  cadastro[:sexo] = gets.chomp.upcase
 
-    cadastro = {}
-    
-    puts "Digite o seu nome: "
-    nome = gets.chomp
+  puts "Digite a idade: "
+  cadastro[:idade] = gets.chomp.to_i
 
-    puts "Digtie o seu sexo[M/F]: "
-    sexo = gets.chomp.upcase
+  soma_idades += cadastro[:idade]
 
-    if cadastro[:sexo] == 'F'
-        cadastro[:mulheres] = nome
-        dados << cadastro[:mulheres]
-    else
-        cadastro[:homens] = nome
-        dados << cadastro[:homens]
-    end
-
-    puts "Digite a sua idade: "
-    idade = gets.chomp.to_i
-
-    if idade >= idade_media
-        idade_media = idade
-        puts idade_media
-    elsif acima_da_media < idade_media
-        idade_media = acima_da_media
-        puts acima_da_media
-    elsif idade_media < acima_da_media
-        acima_da_media = idade_media
-        puts acima_da_media
-    elsif idade <= idade
-        puts "ok"
-    end
+  dados << cadastro
+end
 
 
-    cadastro[:quantidade_cadastrados] = cont
+puts "\nTotal de pessoas cadastradas: #{dados.length}"
 
-    cont += 1
 
+media = soma_idades.to_f / dados.length
+puts "Média de idade: #{media.round(2)}"
+
+
+mulheres = dados.select { |p| p[:sexo] == 'F' }
+puts "\nMulheres cadastradas:"
+mulheres.each { |m| puts m[:nome] }
+
+
+puts "\nPessoas com idade acima da média:"
+dados.each do |p|
+  if p[:idade] > media
+    puts "#{p[:nome]} - #{p[:idade]} anos"
+  end
 end
