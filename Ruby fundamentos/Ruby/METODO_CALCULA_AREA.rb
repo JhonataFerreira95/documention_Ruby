bb68694c0edb4dc2f4515e766e43f646f4006489
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-29 18:30:09 -0300

    finalizado o método area

--- Ruby fundamentos/Ruby/METODO_CALCULA_AREA.rb
+++ Ruby fundamentos/Ruby/METODO_CALCULA_AREA.rb
@@ -4,3 +4,9 @@ Faça um programa que tenha uma função chamada área(),
 que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
 
 =end
+
+def area(largura, comprimento)
+  calculo_base = largura * comprimento
+end
+
+puts area(24.5, 10.2)
