192428ccab84302aeb3b7be0afd6b463bc4db612
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-29 18:51:17 -0300

    tentando nova abordagem

--- Ruby fundamentos/Ruby/MÈTODO_PUTS.rb
+++ Ruby fundamentos/Ruby/MÈTODO_PUTS.rb
@@ -5,3 +5,22 @@ que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho a
 
 =end
 
+def write(* text)
+
+    txt = text.length
+    cont = 0
+    quantidade = 0
+
+    while cont < txt
+      size = text[cont]
+      word = text.length
+      cont += 1
+      puts cont
+    end
+
+    puts "~" * cont
+    puts text
+    puts "~" * cont
+end
+
+puts write("oi")
