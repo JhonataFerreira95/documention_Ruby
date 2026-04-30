f34017132a8c5c35db18ba18c07a5721b37981e5
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-30 08:05:50 -0300

    feito o escopo inicial

--- Ruby fundamentos/Ruby/METODO_MAIOR_MENOR.rb
+++ Ruby fundamentos/Ruby/METODO_MAIOR_MENOR.rb
@@ -4,3 +4,17 @@ Faça um programa que tenha uma função chamada maior(), que receba vários par
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
 =end
+
+def maior(* num)
+    
+end
+
+cont = 0
+
+while true
+
+  puts "Digite o número desejado para comparação: "
+  maior << gets.chomp.to_i
+
+
+end
