ac86c424b6de593a66297e6d4660eb9ff7784b17
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-04-30 23:52:11 -0300

    feito o scopo inicial

--- Ruby fundamentos/Ruby/METODO_SORT_NUMBER.rb
+++ Ruby fundamentos/Ruby/METODO_SORT_NUMBER.rb
@@ -4,3 +4,20 @@ Faça um programa que tenha uma lista chamada números e duas funções chamadas
 A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 
 =end
+
+def sorteia(lista)
+
+    for cont in 0..5
+        lista.insert 
+    end
+
+end
+
+sorteira(num)
+
+num = []
+
+
+def somaPar()
+      
+end
