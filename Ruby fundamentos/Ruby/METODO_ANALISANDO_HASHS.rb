91b108ab1d11d7f6cb106439f05b01261eedcb00
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-02 14:37:24 -0300

    finalizado

--- Ruby fundamentos/Ruby/METODO_ANALISANDO_HASHS.rb
+++ Ruby fundamentos/Ruby/METODO_ANALISANDO_HASHS.rb
@@ -9,3 +9,27 @@ A situação (opcional)
 
 =end
 
+def notas(*n, situacao: false)
+  res = {
+    total: n.size,
+    maior: n.max,
+    menor: n.min,
+    media: (n.sum.to_f / n.size).round(2)
+  }
+
+  if situacao
+    if res[:media] >= 7
+      res[:situacao] = "BOA"
+    elsif res[:media] >= 5
+      res[:situacao] = "RAZOÁVEL"
+    else
+      res[:situacao] = "RUIM"
+    end
+  end
+
+  return res
+end
+
+# Testando a função
+resp = notas(5.5, 2.5, 1.5, situacao: true)
+puts resp
