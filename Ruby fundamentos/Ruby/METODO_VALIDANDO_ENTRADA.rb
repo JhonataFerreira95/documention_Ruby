285f7329d37365dcecc8c11062f2b3b3c598c134
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-02 14:16:06 -0300

    feito o escopo inicial

--- Ruby fundamentos/Ruby/METODO_VALIDANDO_ENTRADA.rb
+++ Ruby fundamentos/Ruby/METODO_VALIDANDO_ENTRADA.rb
@@ -1,6 +1,31 @@
 =begin 
 
 Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 
-‘a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)
+‘a função input() do ruby, só que fazendo a validação para aceitar apenas um valor numérico. 
 
 =end
+
+def leiaInt(msg)
+  
+  ok = false
+  valor = 0
+
+  while true
+    n = gets.chomp(msg)
+
+    if n.to_i
+      valor = n.to_i
+      ok = true
+    else
+      puts "Erro, digite um númeiro inteiro válido!"
+    end
+
+    if ok == true
+      break
+    end
+
+  end
+
+  return valor
+
+end
