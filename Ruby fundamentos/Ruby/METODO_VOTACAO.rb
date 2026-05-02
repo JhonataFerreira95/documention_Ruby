f4af41d4fba60169b21fd2d8f8c8e6a29e99158b
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-02 09:17:48 -0300

    iniciando o método fatorial

---
+++ Ruby fundamentos/Ruby/METODO_VOTACAO.rb
@@ -0,0 +1,23 @@
+=begin
+
+
+
+=end
+# @param i [Integer] inicio da contagem
+# @param f [Integer] fim da contagem
+# @param p [Integer] passo da próxima contagem
+
+
+def contador(i, f, p)
+
+    c = i
+
+    while c <= f
+        puts c
+        c+=p
+    end
+
+end
+
+contador(1, 10, 2)
+
