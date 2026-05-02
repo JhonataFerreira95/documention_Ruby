75b9f468066fa239dc7d3a0316dd882c4823ac7d
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-02 13:21:19 -0300

    escopo inicial

--- Ruby fundamentos/Ruby/METODO_FICHA.rb
+++ Ruby fundamentos/Ruby/METODO_FICHA.rb
@@ -5,3 +5,19 @@ o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de 
 mesmo que algum dado não tenha sido informado corretamente.
 
 =end
+
+def ficha(nome_jogador, gols_marcados)
+    
+end
+
+puts "Digite o nome do jogador: "
+n = gets.chomp
+
+puts "Digite o número de gols: "
+g = gets.chomp
+
+if g.float
+  g = gets.chomp.to_i
+else
+  g = 0
+end
