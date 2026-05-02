4015b240cf920570bff2338d854fd638366971f0
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-02 09:32:51 -0300

    quase pronto

--- Ruby fundamentos/Ruby/METODO_FATORIAL.rb
+++ Ruby fundamentos/Ruby/METODO_FATORIAL.rb
@@ -4,17 +4,22 @@
 
 =end
 
-$global = n
+$n = ' '
 
 
 def fatorial(num=1)
   f = 1
 
-  for c in num..0
+  for c in (1..num).downto(1)
     f*=c
   end
 
-  return
+  return f
 
 end
+
+puts "Digite um número: "
+n = gets.chomp
+
+puts "O fatorial de #{n} é igual a #{fatorial(n)}"
 
