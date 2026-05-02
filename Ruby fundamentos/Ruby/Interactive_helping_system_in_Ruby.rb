85ae27bdef37b3fdbe4afceddb27f2b9568cfe82
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-02 14:41:39 -0300

    feito escopo inicial

--- Ruby fundamentos/Ruby/Interactive_helping_system_in_Ruby.rb
+++ Ruby fundamentos/Ruby/Interactive_helping_system_in_Ruby.rb
@@ -6,3 +6,13 @@ Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importa
 
 =end
 
+def ajuda(com)
+  puts "\e[34m~" * 30
+  puts " ACESSANDO MANUAL DO COMANDO: '#{com}'"
+  puts "~" * 30 + "\e[0m"
+  
+  sleep 1
+
+  system("ri #{com}")
+end
+
