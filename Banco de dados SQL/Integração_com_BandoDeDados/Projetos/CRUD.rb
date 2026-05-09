4c19e6c51fc7acc80e75160a8f6055eb72ad1243
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-09 11:31:17 -0300

    ajustandos a listagem dos dados

--- Banco de dados SQL/Integração_com_BandoDeDados/Projetos/CRUD.rb
+++ Banco de dados SQL/Integração_com_BandoDeDados/Projetos/CRUD.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 conection = PG.connect(
-  dbname: 'cadastro',
+  dbname: 'crud',
   user: 'postgres',
   password: '2319',
   host: 'localhost',
@@ -20,36 +20,23 @@ begin
     nome_usuario = {}
     id = {}
 
-    query_select = 'SELECT * FROM'
-    query_insert = 'INSERT INTO cadastro(login, senha, nome_usuario) VALUES()'
-    query_update = "UPDATE cadastro SET login='#{}', senha='#{}', nome_usuario='#{}'"
-    query_delet = "DELETE FROM cadastro WHERE id=#{}"
 
     puts "Bem vindo ao CRUD básico sobre Cadastro de usuarios!"
 
     sleep 0.5
 
     puts "Selecione uma das operações abaixo: "
-    puts "Ver dados digite [1]: "
-    select = gets.chomp.to_i
-    
-    sleep 0.5
+    puts "  Ver dados digite [1]:\
+    Para inserir dados digite [2]:\
+    Para atualizar dados digite [3]:\
+    Para deleter dados digite [4]: "
+    operacoes = gets.chomp.to_i
 
-    puts "Inserir dados digite [2]: "
-    insert = gets.chomp.to_i
-
-    sleep 0.5
-
-    puts "Para atualizar dados digite [3]: "
-    update = gets.chomp.to_i
-
-    sleep 0.5
-
-    puts "Para deletar dados digite [4]: "
-    delete = gets.chom.to_i
-
-    if select == 1
+    if operacoes == 1
       query_select = 'SELECT * FROM cadastro' 
+      conexao = conection.exec(query_select)
+      puts "Os dados disponiveis são #{query_select}"
+      sleep 1
       puts "Deseja continuar?[S/N] "
       opcao = gets.chomp.upcase
       if  opcao.include?("N")
@@ -57,11 +44,11 @@ begin
       else
         next
       end 
-    elsif insert == 2
+    elsif operacoes == 2
 
-    elsif update == 3
+    elsif operacoes == 3
 
-    elsif delete == 4
+    elsif operacoes == 4
 
     else
       puts "Digite um número válido"
