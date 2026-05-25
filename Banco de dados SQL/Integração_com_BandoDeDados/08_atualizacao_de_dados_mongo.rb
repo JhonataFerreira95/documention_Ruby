d42c62221067861b983a49fe6c95dba8ffc834ac
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-25 01:05:38 -0300

    atualizado dados com mongo

--- Banco de dados SQL/Integração_com_BandoDeDados/08_atualizacao_de_dados_mongo.rb
+++ Banco de dados SQL/Integração_com_BandoDeDados/08_atualizacao_de_dados_mongo.rb
@@ -5,3 +5,15 @@ require 'mongo'
 client = Mongo::Client.new(['localhost:27017'], :database => 'ecommerce')
 
 # Atualização de dados (podemos ter o update_one ou update_many)
+
+result = client[:products].update_one({name: 'smarthphone'}, {
+  "$set" => {category: {name: 'dispositivo móveis'}}
+})
+
+# Verificação do resultado
+
+if result.modified_count > 0
+  puts "Dados atualizados!"
+else
+  puts "Nenhum documentos foi atualizado"
+end
