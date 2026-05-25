cf16a75a3b0b91fecd8ee0aa4e154575dffb85e7
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-25 13:20:46 -0300

    finalizado a exclusão de dados com mongo

--- Banco de dados SQL/Integração_com_BandoDeDados/09_excluindo_dados_mongo.rb
+++ Banco de dados SQL/Integração_com_BandoDeDados/09_excluindo_dados_mongo.rb
@@ -3,3 +3,15 @@ require 'mongo'
 # Configuração do BD
 
 client = Mongo::Client.new(['localhost:27017'], :database => 'ecommerce')
+
+# Exclusão de dados
+
+result = client[:products].delete_one({name: 'smartphone'})
+
+# Verificação da exclusão dos dados
+
+if result.deleted_count > 0
+  puts "Registro excluído com sucesso."
+else
+  puts "nenhum registro excluído!"
+end
