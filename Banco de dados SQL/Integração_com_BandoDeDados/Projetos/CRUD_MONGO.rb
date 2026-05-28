543b11e79b4996d5223b19bd5b5c5e5833129aba
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-28 08:20:06 -0300

    feito a operação de pesquisa

--- Banco de dados SQL/Integração_com_BandoDeDados/Projetos/CRUD_MONGO.rb
+++ Banco de dados SQL/Integração_com_BandoDeDados/Projetos/CRUD_MONGO.rb
@@ -11,6 +11,7 @@ while true
     puts "-=" * 15
     sleep 0.5
 
+
     puts "  Ver dados digite [1]:\
     Para inserir dados digite [2]:\
     Para atualizar dados digite [3]:\
@@ -18,7 +19,26 @@ while true
     operacoes = gets.chomp.to_i
 
     if operacoes == 1
-        
+        puts "Exibindo dados da collection: "
+        sleep 0.5
+
+        puts "-=" * 15
+
+        collection = conection_db[:users]
+        show = collection.find
+
+        puts "-=" * 15
+        sleep 1.0
+
+        puts "Deseja continuar?[S/N] "
+        opcoes = gets.chomp.upcase
+
+        if opcoes.include?("N")
+            break
+        else
+            next
+        end
+
     elsif operacoes == 2
 
     elsif operacoes == 3
