e231ba81718c057483464d48043aaecb4e408f1d
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-05-28 08:09:53 -0300

    feito o escopo inicial

--- Banco de dados SQL/Integração_com_BandoDeDados/Projetos/CRUD_MONGO.rb
+++ Banco de dados SQL/Integração_com_BandoDeDados/Projetos/CRUD_MONGO.rb
@@ -1,3 +1,33 @@
 require 'mongo'
 
 conection_db = Mongo::Connection_db.new(['localhost:27017'], database => 'cadastroDB')
+
+while true
+
+    puts "Bem vindo ao cadastro de usuarios com MongoDB"
+    sleep 0.5
+
+    puts "Selecione a operação desejada: "
+    puts "-=" * 15
+    sleep 0.5
+
+    puts "  Ver dados digite [1]:\
+    Para inserir dados digite [2]:\
+    Para atualizar dados digite [3]:\
+    Para delete dados digite [4]: "
+    operacoes = gets.chomp.to_i
+
+    if operacoes == 1
+        
+    elsif operacoes == 2
+
+    elsif operacoes == 3
+
+    elsif operacoes == 4
+
+    else
+        puts "Digite uma operação válida!"
+        next
+    end
+    
+end
