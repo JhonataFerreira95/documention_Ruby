e338dacc262f7664a5dccc4bf0a43e6623cff949
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-06-30 10:38:19 -0300

    feito a conexão com redis e postgres

--- Banco de dados SQL/redis/redis_CRUD.rb
+++ Banco de dados SQL/redis/redis_CRUD.rb
@@ -1,4 +1,15 @@
 require 'pg'
 require 'redis'
 
-connection = PG.connect
+pg_connection = PG.connect(
+  dbname: 'cadastro',
+  user: 'postgres', 
+  password: '2319',
+  host: 'localhost',
+  port: 5432
+)
+
+redis_connection = Redis.new(
+  host: 'localhost',
+  port: 6379
+)
