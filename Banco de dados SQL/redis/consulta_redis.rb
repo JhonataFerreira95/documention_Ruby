e500cff576a703c3c1a7017a081954ef844ec863
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-07-02 23:16:25 -0300

    feito a conexão

---
+++ Banco de dados SQL/redis/consulta_redis.rb
@@ -0,0 +1,15 @@
+require 'redis'
+require 'pg'
+
+pg_connection = PG.connect(
+  dbname: 'cadastro',
+  user: 'postgres', 
+  password: '2319',
+  host: 'localhost',
+  port: 5432
+)
+
+redis_connection = Redis.new(
+  host: 'localhost',
+  port: 6379
+)
