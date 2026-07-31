6f7ad408109406e586660566b710821ca3958d70
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-07-31 00:25:18 -0300

    usando miscelanea com vogais

--- Ruby avançado/09_miscelanea.rb
+++ Ruby avançado/09_miscelanea.rb
@@ -1,1 +1,11 @@
 # Exemplo de exclusão de caracteres usando expressões regulares
+
+strings = ["apple", "cd", "cherry", "dt", "grape", "fig"]
+
+strings.each do |string|
+   if string =~ /^[^aeiou]+$/
+    puts "#{string} contém apenas consoantes"
+  else
+    puts "#{string} contém vogais"
+  end 
+end
