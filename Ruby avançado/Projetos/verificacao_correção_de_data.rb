a1a0ef2ee5fbfaa4258106afea4d73506b65071d
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-08-02 16:16:04 -0300

    feito o escopo inicial

---
+++ Ruby avançado/Projetos/verificacao_correção_de_data.rb
@@ -0,0 +1,20 @@
+=begin
+
+​Crie um programa que solicite ao usuário uma data no formato "dd/mm/aaaa" e verifique se a entrada corresponde a este formato. 
+Se sim, o programa deve imprimir a data no formato "aaaa-mm-dd". Caso contrário, deve exibir uma mensagem de erro.
+
+=end
+
+
+
+puts "!--------------------------------------------------Bem-vindo ao verificador de data!--------------------------------------------------!"
+
+sleep 1
+
+while true
+
+  puts "Digite a data no formato [dd/mm/aaaa]: "
+
+  
+end
+
