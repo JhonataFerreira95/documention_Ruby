01d86d9a6aee3767db8d14923f268cf9864cdcb9
JhonataFerreira95 <jhonataovirowa@gmail.com>
2026-08-02 20:01:25 -0300

    feito a condicional

--- Ruby avançado/Projetos/verificacao_correção_de_data.rb
+++ Ruby avançado/Projetos/verificacao_correção_de_data.rb
@@ -16,10 +16,16 @@ while true
   puts "Digite a data no formato DIA/MÊS/ANO: "
   usuario_entrada = gets.chomp
 
-  corte = usuario_entrada.split("/")~
-  dia = corte[0].to_i
-  mes = corte[1].to_i
-  ano = corte[2].to_i
+  if usuario_entrada == true
+    corte = usuario_entrada.split("/")
+    dia = corte[0].to_i
+    mes = corte[1].to_i
+    ano = corte[2].to_i
+
+  else
+    puts "Digite um valor que corresponda a DIA/MÊS/ANO."
+    next
+  end
 
 end
 
